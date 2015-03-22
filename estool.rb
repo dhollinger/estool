4840bc6c18a68913742bbfa4debba6806f41c2dd
David Hollinger III <DavidHollinger@solutionary.com>
2015-03-22 15:25:18 -0500

    removed write_log method temporarily

--- estool.rb
+++ estool.rb
@@ -3,10 +3,15 @@ require 'elasticsearch'
 require 'net/http'
 
 class Estool
-  attr_reader :option
+  attr_reader :option, :server
 
-  def write_log
-
+  def initialize(option, server)
+    @option = option
+    @server = server
   end
+
+end
+
+class Connection
 
 end
