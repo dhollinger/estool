08979d45764f423e69a1623f4edb4615e92fa369
David Hollinger III <DavidHollinger@solutionary.com>
2015-05-07 00:02:59 -0500

    Option Parser tests

--- estool.rb
+++ estool.rb
@@ -1,16 +1,35 @@
 #!/usr/bin/env ruby
 require 'elasticsearch'
 require 'net/http'
+require 'optparse'
+require 'ostruct'
+require 'pp'
 
-class Server
-  attr_reader :server
+server = Elasticsearch::Client.new host: 'elasticsrch1:9200'
 
-  def initialize(server)
-    @server = server
+class ElasticOptions
+  def self.parse(args)
+    options = OpenStruct.new
+    options.cat = ""
+
+    optparse = OptionParser.new do |opts|
+      opts.banner = "Usage estool.rb [options]"
+            opts.on('-c', '--cat OPTION', "Must pass a cat parameter") do |c|
+        options.cat = c
+      end
+
+      opts.on_tail('-h', '--help', "Display this screen") do
+        puts opts
+        exit
+      end
+    end
+    optparse.parse!(args)
+    options
   end
-
 end
 
-class Connection < Server
+options = ElasticOptions.new(ARGV)
 
-end
+pp options
+pp ARGV
+
