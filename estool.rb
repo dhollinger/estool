1c731618a21ce74e0d9a44aa6bdddfcfda5bdfb4
David Hollinger <DavidHollinger@solutionary.com>
2015-05-07 14:17:04 -0500

    Removed unused code and added inline documentation

--- estool.rb
+++ estool.rb
@@ -5,38 +5,48 @@ require 'optparse'
 require 'ostruct'
 require 'pp'
 
-server = Elasticsearch::Client.new host: 'esr02sv02:9200'
+# Create Connection to Elasticsearch Nodes
+# Eventually will be an option that defaults to localhost
+server = Elasticsearch::Client.new host: 'esr02sv17:9200'
 
 
 class ElasticOptions
   def self.parse(args)
+    # Creates an OpenStruct Object to store the options in
     options = OpenStruct.new
     options.cat = ''
-    options.nodes = ''
 
     optparse = OptionParser.new do |opts|
+      # Option Banner Shown with the help option
       opts.banner = 'Usage estool.rb [options]'
 
+      # Elasticsearch Cat API Option
       opts.on('-c', '--cat OPTION', 'Must pass a cat parameter') do |c|
         options.cat = c
       end
 
+      # Displays the Help Screen
       opts.on_tail('-h', '--help', 'Display this screen') do
         puts opts
         exit
       end
     end
+    # Parse the optparse variable and return options
     optparse.parse!(args)
     options
   end
 end
 
+# Pass an argument to ElasticOptions class. Results stored in options variable
 options = ElasticOptions.parse(ARGV)
 
+# Check if the Cat option is empty.
 if options.cat != ''
   begin
+    # Attempt to execute a Elasticsearch::API::Cat::CatClient function call
     puts server.cat.send("#{options.cat}", :v => true)
   rescue
-    puts 'Invalid option'
+    # Invalid function passed to Elasticsearch::API::Cat::CatClient
+    puts 'Invalid Elasticsearch Cat option'
   end
 end
