d446e3497fd939cfbff59a07c04fc0c1f2364d01
David Hollinger III <DavidHollinger@solutionary.com>
2015-05-30 19:17:55 -0500

    Removed redundant argument check

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,13 +1,11 @@
 require 'elasticsearch-api'
 
 def get_cat(arg,client)
-  if arg != ''
-    begin
-      # Attempt to execute a Elasticsearch::API::Cat::CatClient function call
-      puts client.cat.send("#{arg}", :v => true)
-    rescue NoMethodError => msg
-      puts msg
-      exit 1
-    end
+  begin
+    # Attempt to execute a Elasticsearch::API::Cat::CatClient function call
+    puts client.cat.send("#{arg}", :v => true)
+  rescue NoMethodError => msg
+    puts msg
+    exit 1
   end
 end
