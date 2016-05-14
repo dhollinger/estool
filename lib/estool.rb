680ffd3fb85324cf847541fc0cade8dda92507e0
David Hollinger III <david.hollinger@egov.com>
2016-05-14 14:47:09 -0500

    Refactor code

--- lib/estool.rb
+++ lib/estool.rb
@@ -2,59 +2,55 @@
 require 'elasticsearch'
 require 'net/http'
 require 'optparse'
-require_relative 'lib/cat'
+require_relative '../lib/estool/cat'
+require_relative '../lib/estool/connections'
 
-options = {
-  cat: '',
-  server: 'localhost',
-  port: '9200'
-}
+module Estool
+  options = {
+      cat: '',
+      server: 'localhost',
+      port: '9200'
+  }
 
-o = OptionParser.new do |opts|
-  # Option Banner Shown with the help option
-  opts.banner = 'Usage estool.rb [options]'
+  o = OptionParser.new do |opts|
+    # Option Banner Shown with the help option
+    opts.banner = 'Usage estool.rb [options]'
 
-  # Elasticsearch Cat API Option
-  opts.on('-c', '--cat OPTION', 'Utilize the Cat API') do |c|
-    options[:cat] = c
+    # Elasticsearch Cat API Option
+    opts.on('-c', '--cat OPTION', 'Utilize the Cat API') do |c|
+      options[:cat] = c
+    end
+
+    # Set Server to connect to. Defaults to localhost.
+    opts.on('-s', '--server OPTION', 'Specify Node to connect to') do |s|
+      options[:server] = s
+    end
+
+    # Set Elasticsearch HTTP port. Defaults to 9200
+    opts.on('-p', '--port OPTION', 'Specify HTTP port') do |p|
+      options[:port] = p
+    end
+
+    # Displays the Help Screen
+    opts.on_tail('-h', '--help', 'Display this screen') do
+      puts opts
+      exit
+    end
   end
 
-  # Set Server to connect to. Defaults to localhost.
-  opts.on('-s', '--server OPTION', 'Specify Node to connect to') do |s|
-    options[:server] = s
+  begin o.parse! ARGV
+  rescue OptionParser::InvalidOption => invopt
+    puts invopt
+    puts o
+    exit 1
+  rescue OptionParser::MissingArgument => misarg
+    puts misarg
+    puts o
+    exit 1
   end
 
-  # Set Elasticsearch HTTP port. Defaults to 9200
-  opts.on('-p', '--port OPTION', 'Specify HTTP port') do |p|
-    options[:port] = p
-  end
+  server = Estool::Connections.start_conn(options[:server], options[:port])
+  Estool::Connections.test_conn(server)
 
-  # Displays the Help Screen
-  opts.on_tail('-h', '--help', 'Display this screen') do
-    puts opts
-    exit
-  end
+  get_cat(options[:cat], server) unless options[:cat] == ''
 end
-
-begin o.parse! ARGV
-rescue OptionParser::InvalidOption => invopt
-  puts invopt
-  puts o
-  exit 1
-rescue OptionParser::MissingArgument => misarg
-  puts misarg
-  puts o
-  exit 1
-end
-
-server = Elasticsearch::Client.new host: "#{options[:server]}:#{options[:port]}"
-
-begin
-  server.cat.health
-rescue Faraday::ConnectionFailed => connfail
-  # Connection Failure Message
-  puts connfail
-  exit 1
-end
-
-get_cat(options[:cat], server) unless options[:cat] == ''
---
+++ lib/estool/connections.rb
@@ -0,0 +1,21 @@
+require 'elasticsearch'
+require 'net/http'
+
+module Estool
+  module Connections
+
+    def self.start_conn(host,port)
+      Elasticsearch::Client.new host: "#{host}:#{port}"
+    end
+
+    def self.test_conn(client)
+      begin
+        client.perform_request 'GET', '_cluster/health'
+      rescue Faraday::ConnectionFailed => connfail
+        # Connection Failure Message
+        puts connfail
+        exit 1
+      end
+    end
+  end
+end
