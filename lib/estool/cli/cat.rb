e878281dc0c5cc09580df0edff526d312ac2cb2f
David Hollinger III <david.hollinger@egov.com>
2016-05-23 19:55:58 -0500

    Add code to take into accound command changes.
    Added code that accound for the new command parameter when the
    class is being called by the CLI class. Added code to account for
    when the verbose option is used as not all subcommands support it.

--- lib/estool/cli/cat.rb
+++ lib/estool/cli/cat.rb
@@ -3,9 +3,10 @@ require 'lib/estool/connections'
 
 module Estool
   class Cli::Cat
-    attr_reader :options
+    attr_reader :command, :options
 
-    def initialize(options)
+    def initialize(command, options)
+      @command = command
       @options = options
     end
 
@@ -13,9 +14,16 @@ module Estool
       client = Estool::Connections.start_conn(@options[:host],@options[:port])
       Estool::Connections.test_conn(client)
       # Attempt to execute a Elasticsearch::API::Cat::CatClient function call
-      puts client.cat.send("#{@options[:cmd]}", v: true)
-    rescue NoMethodError => msg
-      puts msg
+      if @options[:verbose] == true
+        puts client.cat.send("#{@command}", master_timeout: 30, v: true)
+      else
+        puts client.cat.send("#{@command}", master_timeout: 30)
+      end
+    rescue NoMethodError
+      puts "Unsupported command: #{@command}"
+      exit 1
+    rescue ArgumentError => arg
+      puts arg
       exit 1
     end
 
