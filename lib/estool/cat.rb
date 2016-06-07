1052681dc3f065907c7d8e8f1a322765c0be8fae
David Hollinger III <david.hollinger@egov.com>
2016-06-07 00:13:32 -0500

    Move common options to class_option

--- lib/estool/cat.rb
+++ lib/estool/cat.rb
@@ -2,27 +2,28 @@ require 'thor'
 
 module Estool
   class Cat < Thor
-    desc 'aliases [OPTIONS]', 'Display information about aliases'
-    method_option 'host', :type => :string,
+    class_option 'host', :type => :string,
                   :banner => 'Elasticsearch host to connect to',
                   :default => 'localhost',
                   :aliases => '-H'
-    method_option 'port', :type => :string,
+    class_option 'port', :type => :string,
                   :banner => 'Port to use when connecting to Elasticsearch',
                   :default => '9200',
                   :aliases => '-p'
-    method_option 'verbose', :type => :boolean,
+    class_option 'verbose', :type => :boolean,
                   :banner => 'Verbose option. Not all subcommands support it',
                   :default => false,
                   :aliases => '-v'
-    method_option 'output', :type => :string,
+    class_option 'output', :type => :string,
                   :banner => 'Display output as json or text',
                   :default => 'text',
                   :aliases => '-o'
-    method_option 'timeout', :type => :numeric,
+    class_option 'timeout', :type => :numeric,
                   :banner => 'Set Master Timeout in seconds',
                   :default => 30,
                   :aliases => '-t'
+
+    desc 'aliases [OPTIONS]', 'Display information about aliases'
     method_option 'name', :type => :string,
                   :banner => 'Name of alias(es) to display information about',
                   :aliases => '-n'
@@ -32,26 +33,6 @@ module Estool
     end
 
     desc 'allocation [OPTIONS]', 'Display shard allocation information'
-    method_option 'host', :type => :string,
-                  :banner => 'Elasticsearch host to connect to',
-                  :default => 'localhost',
-                  :aliases => '-H'
-    method_option 'port', :type => :string,
-                  :banner => 'Port to use when connecting to Elasticsearch',
-                  :default => '9200',
-                  :aliases => '-p'
-    method_option 'verbose', :type => :boolean,
-                  :banner => 'Verbose option. Not all subcommands support it',
-                  :default => false,
-                  :aliases => '-v'
-    method_option 'output', :type => :string,
-                  :banner => 'Display output as json or text',
-                  :default => 'text',
-                  :aliases => '-o'
-    method_option 'timeout', :type => :numeric,
-                  :banner => 'Set Master Timeout in seconds',
-                  :default => 30,
-                  :aliases => '-t'
     method_option 'bytes', :type => :string,
                   :banner => 'Unit to display byte values',
                   :default => 'b',
@@ -66,52 +47,12 @@ module Estool
     end
 
     desc 'health [OPTIONS]', 'Display Elasticsearch Cluster health'
-    method_option 'host', :type => :string,
-                  :banner => 'Elasticsearch host to connect to',
-                  :default => 'localhost',
-                  :aliases => '-H'
-    method_option 'port', :type => :string,
-                  :banner => 'Port to use when connecting to Elasticsearch',
-                  :default => '9200',
-                  :aliases => '-p'
-    method_option 'verbose', :type => :boolean,
-                  :banner => 'Verbose option. Not all subcommands support it',
-                  :default => false,
-                  :aliases => '-v'
-    method_option 'output', :type => :string,
-                  :banner => 'Display output as json or text',
-                  :default => 'text',
-                  :aliases => '-o'
-    method_option 'timeout', :type => :numeric,
-                  :banner => 'Set Master Timeout in seconds',
-                  :default => 30,
-                  :aliases => '-t'
     def health
       require 'lib/estool/cat/health'
       Health.new(options).run
     end
 
     desc 'nodes [OPTIONS]', 'Display Elasticsearch Nodes'
-    method_option 'host', :type => :string,
-                  :banner => 'Elasticsearch host to connect to',
-                  :default => 'localhost',
-                  :aliases => '-H'
-    method_option 'port', :type => :string,
-                  :banner => 'Port to use when connecting to Elasticsearch',
-                  :default => '9200',
-                  :aliases => '-p'
-    method_option 'verbose', :type => :boolean,
-                  :banner => 'Verbose option. Not all subcommands support it',
-                  :default => false,
-                  :aliases => '-v'
-    method_option 'output', :type => :string,
-                  :banner => 'Display output as json or text',
-                  :default => 'text',
-                  :aliases => '-o'
-    method_option 'timeout', :type => :numeric,
-                  :banner => 'Set Master Timeout in seconds',
-                  :default => 30,
-                  :aliases => '-t'
     def nodes
       require 'lib/estool/cat/nodes'
       Nodes.new(options).run
