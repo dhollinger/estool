bbdb936570cb4ec30d31803db01ea5bfe700abbe
David Hollinger III <david.hollinger@egov.com>
2016-06-24 15:25:56 -0500

    Add universal class for Cat actions

---
+++ lib/estool/actions/cat.rb
@@ -0,0 +1,20 @@
+require 'elasticsearch-api'
+require 'lib/estool/connections'
+
+module Estool::Actions
+  class Cat
+    def self.run(action, data, options)
+      client = Estool::Connections.start_conn(options[:host], options[:port])
+      Estool::Connections.test_conn(client)
+      begin
+        puts client.cat.send(action, data)
+      rescue ArgumentError => args
+        puts "
+             #{args}
+        Usage: 'estool cat help #{action}' for more information
+        "
+        exit 1
+      end
+    end
+  end
+end
