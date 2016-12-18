a5160b27390dd93e5c3911c6eb01a95b4b69d87c
David Hollinger III <david.hollinger@egov.com>
2016-12-17 21:57:20 -0600

    Add create and remove subcommands

--- lib/estool/cli/aliases.rb
+++ lib/estool/cli/aliases.rb
@@ -28,6 +28,50 @@ module Estool
           ls_alias.run
         end
       end
+
+      c.desc 'create an alias for an index.'
+      c.command [:create, :c] do |create|
+        create.flag [:name, :n], :arg_name => 'alias name',
+                                 :required => true,
+                                 :desc     => 'What to name the alias.'
+
+        create.flag [:index, :i], :arg_name => 'index name',
+                                  :required => true,
+                                  :desc     => 'Index to apply alias to.'
+
+        create.action do |global_options,options,args|
+          options = {
+            host: global_options[:host],
+            port: global_options[:port],
+            name: options[:name],
+            index: options[:index]
+          }
+          cr_alias = Estool::Actions::Index.new('put_alias', options)
+          cr_alias.run
+        end
+      end
+
+      c.desc 'delete an alias for an index.'
+      c.command [:delete, :d] do |delete|
+        delete.flag [:name, :n], :arg_name => 'alias name',
+                                 :required => true,
+                                 :desc     => 'Which alias to delete.'
+
+        delete.flag [:index, :i], :arg_name => 'index name',
+                                  :required => true,
+                                  :desc     => 'Index to apply alias to.'
+
+        delete.action do |global_options,options,args|
+          options = {
+            host: global_options[:host],
+            port: global_options[:port],
+            name: options[:name],
+            index: options[:index]
+          }
+          dl_alias = Estool::Actions::Index.new('delete_alias', options)
+          dl_alias.run
+        end
+      end
     end
   end
 end
